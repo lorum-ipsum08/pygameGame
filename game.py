2a9e0d1a996cf8772f815d7cbaac44bb690ec6f0
lorum-ipsum08 <56936484+lorum-ipsum08@users.noreply.github.com>
2022-12-21 23:16:08 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -1,8 +1,9 @@
 import pygame as pg
 from random import randint as rng
+pg.init()
 screensize = (1200, 500)
 score = 0
-vel = 5
+vel = 10
 width = 1200
 height = 500
 isJump = False
@@ -13,10 +14,12 @@ obstXY = (100, 100)
 man = pg.image.load(r"man-standing.png")
 all_sprites_list = pg.sprite.Group()
 floorPos = pg.Rect(0, screensize[1] - 25, screensize[0], 25)
+background = pg.image.load(r"background.png")
+speeds = [(5,1),(10,2),(15,3),(20,5)]
 
-pg.init()
 
 font = pg.font.Font('freesansbold.ttf', 32)
+smallfont = pg.font.Font('freesansbold.ttf', 16)
 text = font.render(str(score), True, 'black')
 text_rect = text.get_rect()
 text_rect[0] = screensize[0] - text_rect[2]
@@ -26,20 +29,46 @@ screen = pg.display.set_mode(size=screensize)
 
 
 text = font.render('Press E To Start!', True, 'black')
-text_rect = text.get_rect(center=(screensize[0]/2, screensize[1]/2))
-screen.fill('white')
+modeText = smallfont.render('Press 1, 2, 3, or 4 for Easy, Mid, Hard, or Hyper', True, 'black')
+text_rect = text.get_rect(center=(screensize[0]/2, 60))
+mode_Rect = modeText.get_rect()
+mode_Rect[0] = 5
+mode_Rect[1] = 5
+screen.blit(background, (0, 0))
+screen.blit(modeText, mode_Rect)
 screen.blit(text, text_rect)
 text_rect[0] = screensize[0] - text_rect[2]
 pg.display.flip()
 
 loading = True
 while loading:
+    mode_Rect = modeText.get_rect()
+    mode_Rect[0] = 5
+    mode_Rect[1] = 5
+    text_rect[0] = screensize[0]/2 - text_rect[2]/2
+    screen.blit(background, (0, 0))
+    screen.blit(modeText, mode_Rect)
+    screen.blit(text, text_rect)
     for i in pg.event.get():
         if i.type == pg.QUIT:
             pg.quit()
     keys = pg.key.get_pressed()
     if keys[pg.K_e]:
         loading = False
+    if keys[pg.K_1]:
+        modeText = font.render('Easy', True, 'black')
+        mode = 0
+    elif keys[pg.K_2]:
+        modeText = font.render('Mid', True, 'black')
+        mode = 1
+    elif keys[pg.K_3]:
+        modeText = font.render('Hard', True, 'black')
+        mode = 2
+    elif keys[pg.K_4]:
+        modeText = font.render('Hyper', True, 'black')
+        mode = 3
+    pg.display.flip()
+
 
 def generate_obstacle():
     num = rng(1, 6)
@@ -76,17 +105,18 @@ def generate_flying_obstacle():
         return -1
     return obst
 
-
+    
 def update_screen(scrn, spritelist):
     screen.fill(0x87CEEB)
+    screen.blit(background, (0, 0))
     spritelist.draw(scrn)
-    pg.draw.rect(scrn, 0x9b7653, floorPos)
+    pg.draw.rect(scrn, 0x7CFC00, floorPos)
     text = font.render(str(score), True, 'black', 0x87CEEB)
     text_rect = text.get_rect()
     text_rect[0] = screensize[0] - text_rect[2]
     screen.blit(text, text_rect)
     pg.display.flip()
-
+    
 
 def switch_image(obj, new_image):
     obj.image.fill('brown')
@@ -215,12 +245,15 @@ player.rect.x, player.rect.y = 100, screensize[1] - 160 - 20
 all_sprites_list.add(player)
 
 while run:
-    update_screen(screen, all_sprites_list)
-    pg.time.delay(17)
-    score += 1
     
-    Flying_Obstacle.rect.x -= 10
-    obstacle.rect.x -= 10
+    print((keys[pg.K_DOWN] or keys[pg.K_s]))
+    
+    pg.time.delay(17)
+    score += speeds[mode][1]
+    
+    Flying_Obstacle.rect.x -= speeds[mode][0]
+    obstacle.rect.x -= speeds[mode][0]
+    
     if detect_collision(player, obstacle, False) == 1:
         dead = damage((life1, life2, life3))
         if dead:
@@ -244,45 +277,47 @@ while run:
 
     keys = pg.key.get_pressed()
 
-    if keys[pg.K_LEFT] and player.rect.x > 0:
+    if (keys[pg.K_LEFT] or keys[pg.K_a]) and player.rect.x > 0:
         player.rect.x -= vel
 
-    if keys[pg.K_RIGHT] and player.rect.x < screensize[0] - 120:
+    if (keys[pg.K_RIGHT] or keys[pg.K_d]) and player.rect.x < screensize[0] - 120:
         player.rect.x += vel
-    if keys[pg.K_UP] and not isJump:
+        
+    if (keys[pg.K_UP] or keys[pg.K_w]) and not isJump:
         upPressed = True
-    if keys[pg.K_DOWN] and not isJump:
+        
+    if (keys[pg.K_DOWN] or keys[pg.K_s]) and not isJump:
         man = pg.image.load(r"man-ducking.png")
         switch_image(player, man)
         player.ducking = True
-    if not keys[pg.K_DOWN] and not isJump:
+        
+    if (not keys[pg.K_DOWN] and not keys[pg.K_s]) and not isJump:
         man = pg.image.load(r"man-standing.png")
         switch_image(player, man)
         player.ducking = False
+        
     if not isJump and upPressed:
+        
         if jumpCount >= -10:
             man = pg.image.load(r"man-jumping.png")
             switch_image(player, man)
             player.rect.y -= (jumpCount * abs(jumpCount)) * 0.5
-            update_screen(screen, all_sprites_list)
             jumpCount -= 0.5
+            
         elif jumpCount == -11:
             player.rect.y += 5
             man = pg.image.load(r"man-standing.png")
             switch_image(player, man)
-            update_screen(screen, all_sprites_list)
             jumpCount = 10
             isJump = False
             upPressed = False
+            
         else:
             man = pg.image.load(r"man-standing.png")
             switch_image(player, man)
-            update_screen(screen, all_sprites_list)
             jumpCount = 10
             isJump = False
             upPressed = False
-    else:
-        continue
 
     all_sprites_list.update()
     update_screen(screen, all_sprites_list)
