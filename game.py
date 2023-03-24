c06670420886d66a7d234cbb19e1c1b3679f9b8e
lorum-ipsum08 <56936484+lorum-ipsum08@users.noreply.github.com>
2023-03-24 21:36:54 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -1,6 +1,34 @@
 import pygame as pg
-from random import randint as rng
-pg.init()
+from random import randint
+
+pg.display.init()
+pg.font.init()
+
+font = pg.font.Font('freesansbold.ttf', 32)
+smallfont = pg.font.Font('freesansbold.ttf', 16)
+
+def render_lrg_text(txt, hex, pos, centered):
+    txt = font.render(txt, True, hex)
+    if not centered:
+        txt_rect = txt.get_rect()
+        txt_rect[0] = pos[0]
+        txt_rect[1] = pos[1]
+    if centered:
+        txt_rect = txt.get_rect(center=pos)
+    screen.blit(txt, txt_rect)
+
+def render_sml_text(txt, hex, pos, centered):
+    txt = smallfont.render(txt, True, hex)
+    if not centered:
+        txt_rect = txt.get_rect()
+        txt_rect[0] = pos[0]
+        txt_rect[1] = pos[1]
+    if centered:
+        txt_rect = txt.get_rect(center=pos)
+        txt_rect[0] = 20
+        txt_rect[1] = 20
+    screen.blit(txt, txt_rect)
+
 screensize = (1200, 500)
 score = 0
 vel = 10
@@ -15,9 +43,9 @@ man = pg.image.load(r"man-standing.png")
 all_sprites_list = pg.sprite.Group()
 floorPos = pg.Rect(0, screensize[1] - 25, screensize[0], 25)
 background = pg.image.load(r"background.png")
-speeds = [(5,1),(10,2),(15,3),(20,5)]
-
-
+speeds = [(20,1),(25,2),(30,3),(40,5),(50,10)]
+mode = -1
+restingpos = screensize[1] - 25 - 160 + 5 
 font = pg.font.Font('freesansbold.ttf', 32)
 smallfont = pg.font.Font('freesansbold.ttf', 16)
 text = font.render(str(score), True, 'black')
@@ -28,50 +56,70 @@ text_rect[0] = screensize[0] - text_rect[2]
 screen = pg.display.set_mode(size=screensize)
 
 
-text = font.render('Press E To Start!', True, 'black')
-modeText = smallfont.render('Press 1, 2, 3, or 4 for Easy, Mid, Hard, or Hyper', True, 'black')
-text_rect = text.get_rect(center=(screensize[0]/2, 60))
-mode_Rect = modeText.get_rect()
-mode_Rect[0] = 5
-mode_Rect[1] = 5
 screen.blit(background, (0, 0))
-screen.blit(modeText, mode_Rect)
-screen.blit(text, text_rect)
-text_rect[0] = screensize[0] - text_rect[2]
+render_sml_text('Press 1, 2, 3, Or 4 For Easy, Mid, Hard, Or Hyper', 0x000000, (5,5), False)
+render_lrg_text('Press E To Start', 0x000000, (screensize[0]/2, 60), True)
+
 pg.display.flip()
 
 loading = True
 while loading:
-    mode_Rect = modeText.get_rect()
-    mode_Rect[0] = 5
-    mode_Rect[1] = 5
-    text_rect[0] = screensize[0]/2 - text_rect[2]/2
+    
     screen.blit(background, (0, 0))
-    screen.blit(modeText, mode_Rect)
-    screen.blit(text, text_rect)
+    
+    render_lrg_text('Press E To Start', 0x000000, (screensize[0]/2, 60), True)
+    
+    render_sml_text('Press 1, 2, 3, Or 4 For Easy, Mid, Hard, Or Hyper', 0x000000, (5,5), False)
+        
     for i in pg.event.get():
         if i.type == pg.QUIT:
-            pg.quit()
+            quit()
+            
     keys = pg.key.get_pressed()
-    if keys[pg.K_e]:
+    
+    if keys[pg.K_e] and mode != -1:
         loading = False
-    if keys[pg.K_1]:
-        modeText = font.render('Easy', True, 'black')
+        
+    elif keys[pg.K_1]:
         mode = 0
+        
     elif keys[pg.K_2]:
-        modeText = font.render('Mid', True, 'black')
         mode = 1
+        
     elif keys[pg.K_3]:
-        modeText = font.render('Hard', True, 'black')
         mode = 2
+        
     elif keys[pg.K_4]:
-        modeText = font.render('Hyper', True, 'black')
         mode = 3
+        
+    elif keys[pg.K_g] and keys[pg.K_a] and keys[pg.K_y]:
+        mode = 4
+        
+    if not mode == 0:
+        render_lrg_text('EASY', 0x000000, (screensize[0] - 75, 20), True)
+    if not mode == 1:
+        render_lrg_text('MID', 0x000000, (screensize[0] - 75, 50), True)
+    if not mode == 2:
+        render_lrg_text('HARD', 0x000000, (screensize[0] - 75, 80), True)
+    if not mode == 3:
+        render_lrg_text('HYPER', 0x000000, (screensize[0] - 75, 110), True) 
+        
+    if mode == 0:
+        render_lrg_text('|EASY|', 0x000000, (screensize[0] - 75, 20), True)
+    elif mode == 1:
+        render_lrg_text('|MID|', 0x000000, (screensize[0] - 75, 50), True)
+    elif mode == 2:
+        render_lrg_text('|HARD|', 0x000000, (screensize[0] - 75, 80), True)
+    elif mode == 3:
+        render_lrg_text('|HYPER|', 0x000000, (screensize[0] - 75, 110), True)
+    elif mode == 4:
+        render_lrg_text('|?????|', 0x000000, (screensize[0] - 75, 140), True)
+        
     pg.display.flip()
 
 
 def generate_obstacle():
-    num = rng(1, 6)
+    num = randint(1, 6)
     if num == 1:
         obst = pg.image.load(r"tablesawblade.png")
     elif num == 2:
@@ -90,7 +138,7 @@ def generate_obstacle():
 
 
 def generate_flying_obstacle():
-    num = rng(1, 5)
+    num = randint(1, 5)
     if num == 1:
         obst = pg.image.load(r"flyingloan.png")
     elif num == 2:
@@ -107,14 +155,19 @@ def generate_flying_obstacle():
 
     
 def update_screen(scrn, spritelist):
-    screen.fill(0x87CEEB)
+    screen.fill('brown')
+    
     screen.blit(background, (0, 0))
+    
     spritelist.draw(scrn)
+    
     pg.draw.rect(scrn, 0x7CFC00, floorPos)
-    text = font.render(str(score), True, 'black', 0x87CEEB)
+    
+    text = font.render(str(score), True, 0x000000)
     text_rect = text.get_rect()
     text_rect[0] = screensize[0] - text_rect[2]
     screen.blit(text, text_rect)
+    
     pg.display.flip()
     
 
@@ -245,14 +298,13 @@ player.rect.x, player.rect.y = 100, screensize[1] - 160 - 20
 all_sprites_list.add(player)
 
 while run:
-    
-    print((keys[pg.K_DOWN] or keys[pg.K_s]))
-    
+        
     pg.time.delay(17)
     score += speeds[mode][1]
     
     Flying_Obstacle.rect.x -= speeds[mode][0]
     obstacle.rect.x -= speeds[mode][0]
+    
     
     if detect_collision(player, obstacle, False) == 1:
         dead = damage((life1, life2, life3))
@@ -265,23 +317,31 @@ while run:
             run = False
 
     if obstacle.rect.x <= 0 and Flying_Obstacle.rect.x <= 0:
-        choice = rng(1, 2)
+        choice = randint(1, 2)
         if choice == 1:
             respawn_obstacle(obstacle)
-        else:
+        if choice == 2:
             respawn_flying_obstacle(Flying_Obstacle)
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            run = False
+            quit()
 
     keys = pg.key.get_pressed()
-
+    
+    if keys[pg.K_p]:
+        paused = True
     if (keys[pg.K_LEFT] or keys[pg.K_a]) and player.rect.x > 0:
-        player.rect.x -= vel
+        if player.ducking:
+            player.rect.x -= vel/2
+        else:
+            player.rect.x -= vel
 
     if (keys[pg.K_RIGHT] or keys[pg.K_d]) and player.rect.x < screensize[0] - 120:
-        player.rect.x += vel
+        if player.ducking:
+            player.rect.x += vel/2
+        else:
+            player.rect.x += vel
         
     if (keys[pg.K_UP] or keys[pg.K_w]) and not isJump:
         upPressed = True
@@ -296,16 +356,21 @@ while run:
         switch_image(player, man)
         player.ducking = False
         
+    if (keys[pg.K_DOWN] or keys[pg.K_s]) and player.rect.y != restingpos:
+        player.rect.y = restingpos
+        man = pg.image.load(r"man-ducking.png") # un hardcode
+        jumpCount = 10
+        upPressed = False
+        score
     if not isJump and upPressed:
-        
         if jumpCount >= -10:
             man = pg.image.load(r"man-jumping.png")
             switch_image(player, man)
-            player.rect.y -= (jumpCount * abs(jumpCount)) * 0.5
-            jumpCount -= 0.5
-            
-        elif jumpCount == -11:
-            player.rect.y += 5
+            player.rect.y -= (jumpCount * abs(jumpCount)) * 0.4
+            jumpCount -= 0.8
+                        
+        elif jumpCount <= -11:
+            player.rect.y = restingpos
             man = pg.image.load(r"man-standing.png")
             switch_image(player, man)
             jumpCount = 10
@@ -313,6 +378,7 @@ while run:
             upPressed = False
             
         else:
+            player.rect.y = restingpos
             man = pg.image.load(r"man-standing.png")
             switch_image(player, man)
             jumpCount = 10
@@ -324,18 +390,21 @@ while run:
 
 
 
+smthn = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'
+smthn = smthn.split(' ')
 
-text = font.render(f'GAME OVER | You survived {score} updates', True, 'black')
-text_rect = text.get_rect(center=(screensize[0]/2, screensize[1]/2))
 while True:
     
+    num = randint(0,25)
     screen.fill('white')
-    screen.blit(text, text_rect)
+    render_lrg_text(f'{smthn[num]} GAME OVER | You Got {score} Score! {smthn[num]}', 0x000000, (screensize[0]/2,screensize[1]/2), True)
     pg.display.flip()
+    
     for event in pg.event.get():
     
         if event.type == pg.QUIT:
             pg.quit()
             quit()
 
-        pg.display.update()
+        pg.display.flip()
+        
